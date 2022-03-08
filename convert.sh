8d87ade25cadf3c0a303d094fd65e2442df3f1ce
Sebastien Wains <swains@redhat.com>
2022-03-08 12:15:24 +0100

    add missing crt

--- convert.sh
+++ convert.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# If needed fix your CentOS repo first
+# sed -i 's/^mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+# sed -i 's|#baseurl=http://mirror.centos.org|baseurl=https://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
 if [ ! -x /usr/bin/curl ]; then
     dnf -y install curl
 fi
@@ -10,8 +14,9 @@ read USERNAME
 echo "What's your Red Hat password? "
 read -s PASSWORD 
 
-
 curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release https://www.redhat.com/security/data/fd431d51.txt
+
+curl --create-dirs -o /etc/rhsm/ca/redhat-uep.pem https://ftp.redhat.com/redhat/convert2rhel/redhat-uep.pem
 
 curl -o /etc/yum.repos.d/convert2rhel.repo https://ftp.redhat.com/redhat/convert2rhel/8/convert2rhel.repo
 
